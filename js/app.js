39510651ce1a81a2391664ae13c366067aca1661
Adam <acdoyle630@gmail.com>
2017-05-09 19:19:27 -1000

    render hello tasks

--- js/app.js
+++ js/app.js
@@ -1,1 +1,68 @@
-console.log('Testing');
+const assignedContainer = document.getElementById('assigned');
+
+const getTasks = () => new Promise((resolve, reject) =>{
+  const tasksFromGetTasks = [
+      {
+        name: "test task"
+      }
+    ];
+    setTimeout(() => resolve(
+      tasksFromGetTasks),250);
+});
+
+const Task = (props) => (
+  <li>
+    <h3>{ props.task.name }</h3>
+  </li>
+);
+
+
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      tasks: [],
+    };
+    //this.addTask = this.addTask.bind(this);
+
+  }
+
+  componentWillMount() {
+    this.getFaketasks()
+      .then(tasks =>{
+        this.setState({ tasks });
+      });
+    }
+    getFaketasks(){
+      let value =  getTasks();
+      return value;
+    }
+
+  render(){
+    return(
+      <div>
+        <h1>Hello Tasks</h1>
+        <div tasks={this.state.tasks}></div>
+      </div>
+    );
+  }
+};
+
+ReactDOM.render(
+
+  <App />,
+
+  assignedContainer
+
+);
+
+
+
+
+
+
+
+
+
