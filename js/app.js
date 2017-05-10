a254d1a49fc7547dd7c93620bb1a8e5b31b218fa
Adam <acdoyle630@gmail.com>
2017-05-10 11:57:36 -1000

    able to add card

--- js/app.js
+++ js/app.js
@@ -1,20 +1,161 @@
-const assignedContainer = document.getElementById('assigned');
+const queueContainer = document.getElementById('queue');
+const inProgressContainer = document.getElementById('inProgress');
+const done = document.getElementById('done');
 
-const getTasks = () => new Promise((resolve, reject) =>{
-  const tasksFromGetTasks = [
+const getCards = () => new Promise((resolve, reject) =>{
+  const cardsFromGetCards = [
       {
-        name: "test task"
+        id: 1,
+        title: "test task",
+        priority: "High",
+        status: "Queue",
+        created_by: "Adam",
+        assigned_to: "Bob"
+      },
+      {
+        id: 2,
+        title: "Laundry",
+        priority: "Medium",
+        status: "Queue",
+        created_by: "Adam",
+        assigned_to: "Kat"
       }
     ];
     setTimeout(() => resolve(
-      tasksFromGetTasks),250);
+      cardsFromGetCards),250);
 });
 
-const Task = (props) => (
+const Card = (props) => (
   <li>
-    <h3>{ props.task.name }</h3>
+    <h3>Title: { props.card.title }</h3>
+    <p>id: { props.card.id }</p>
+    <p>Priority: { props.card.priority }</p>
+    <p>Status: { props.card.status}</p>
+    <p>Created by: { props.card.created_by}</p>
+    <p>Assigned to: { props.card.assigned_to}</p>
   </li>
 );
+
+
+const CardList = ({ cards }) =>(
+  <ul>
+    {  cards.map(card => <Card card={card} /> ) }
+  </ul>
+  );
+
+let id = 3;
+
+class NewCardForm extends React.Component {
+  constructor(props){
+
+    super(props);
+
+    this.state = {
+      id: id,
+      title: "",
+      priority: "",
+      status: "Queue",
+      created_by: "",
+      assigned_to: ""
+    };
+
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+
+    this.handlePriorityChange = this.handlePriorityChange.bind(this);
+
+    this.handleCreatedByChange = this.handleCreatedByChange.bind(this);
+
+    this.handleAssignedToChange = this.handleAssignedToChange.bind(this);
+  }
+
+    addCard(card){
+      console.log(card);
+      this.props.addCard(card);
+      const title = "";
+      const priority = "";
+      const created_by = "";
+      const assigned_to = "";
+      this.setState({
+        id,
+        title,
+        priority,
+        status,
+        created_by,
+        assigned_to
+      });
+    }
+
+    handleSubmit(event) {
+      console.log('hit handle submit');
+      id++;
+      event.preventDefault();
+      console.log(this.state);
+      this.addCard(this.state);
+    }
+
+    // handleIdChange(event) {
+    //   console.log('hit handled id change')
+    //   console.log(this.state)
+    //   event.preventDefault();
+    //   this.setState({ id: event.target.value });
+    // }
+    handleTitleChange(event){
+      console.log('hit handle title change')
+      event.preventDefault();
+      this.setState({ title: event.target.value });
+    }
+    handleStatusChange(event){
+      console.log('hit handle status change')
+      event.preventDefault();
+      this.setState({ status: event.target.value});
+    }
+    handlePriorityChange(event){
+      console.log('hit handle priority change')
+      event.preventDefault();
+      this.setState({ priority: event.target.value });
+    }
+    handleCreatedByChange(event) {
+      console.log('hit handle created by change')
+      event.preventDefault();
+      this.setState({ created_by: event.target.value });
+    }
+
+    handleAssignedToChange(event){
+      console.log('hit handle assigned to change')
+      event.preventDefault();
+      this.setState({ assigned_to: event.target.value});
+    }
+    render(){
+      return(
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input type="text" placeholder="title" onChange={this.handleTitleChange} value={this.state.title} />
+          </div>
+
+          <div>
+            <input type="text" placeholder="Priority" onChange={this.handlePriorityChange} value={this.state.priority} />
+          </div>
+
+          <div>
+            <input type="text" placeholder="Created By" onChange={this.handleCreatedByChange} value={this.state.created_by} />
+          </div>
+
+          <div>
+            <input type="text" placeholder="Assigned To" onChange={this.handleAssignedToChange} value={this.state.assigned_to} />
+          </div>
+
+          <div>
+            <button type="submit">Add Card</button>
+          </div>
+        </form>
+        )
+    }
+  }
+
+
 
 
 class App extends React.Component{
@@ -23,28 +164,33 @@ class App extends React.Component{
     super(props);
 
     this.state = {
-      tasks: [],
+      cards: [],
     };
-    //this.addTask = this.addTask.bind(this);
-
+    this.addCard = this.addCard.bind(this);
   }
-
   componentWillMount() {
-    this.getFaketasks()
-      .then(tasks =>{
-        this.setState({ tasks });
+    this.getFakeCards()
+      .then(cards =>{
+        this.setState({ cards });
       });
     }
-    getFaketasks(){
-      let value =  getTasks();
-      return value;
-    }
+
+  addCard(card){
+    this.setState({
+      cards : this.state.cards.concat(card)
+    });
+  }
+
+  getFakeCards(){
+    return  getCards();
+  }
 
   render(){
     return(
       <div>
-        <h1>Hello Tasks</h1>
-        <div tasks={this.state.tasks}></div>
+        <h1>Hello Cards</h1>
+        <CardList cards={this.state.cards}></CardList>
+        <NewCardForm addCard={this.addCard}/>
       </div>
     );
   }
@@ -54,7 +200,7 @@ ReactDOM.render(
 
   <App />,
 
-  assignedContainer
+  queueContainer
 
 );
 
